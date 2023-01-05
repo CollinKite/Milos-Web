98f5ff2516e5ce9534954d9864b9c363b8a97cac
Collin Kite <me@collinkite.com>
2023-01-05 10:11:41 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <input type="file"
+       id="video" name="video"
+       accept="image/png, image/jpeg"></input>
+    </div>
+  );
+}
+
+export default App;
